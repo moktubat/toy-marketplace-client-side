2d4837028188ba20ddb8050af1ab181e0076da54
moktubat <shadhinreja@gmail.com>
2023-05-20 05:44:27 +0600

    image added in error page

--- src/Layout/pages/ErrorPage/ErrorPage.jsx
+++ src/Layout/pages/ErrorPage/ErrorPage.jsx
@@ -1,38 +1,28 @@
 const ErrorPage = () => {
   return (
-    <section className="bg-primary relative z-10 h-screen">
-      <div className="container mx-auto">
-        <div className="-mx-4 flex">
-          <div className="w-full px-4 mt-60">
-            <div className="mx-auto max-w-[400px] text-center">
-              <h2 className="mb-2 text-[50px] font-bold leading-none text-white sm:text-[80px] md:text-[100px]">
-                404
-              </h2>
-              <h4 className="mb-3 text-[22px] font-semibold leading-tight text-white">
-                Oops! That page can’t be found
-              </h4>
-              <p className="mb-8 text-lg text-white">
-                The page you are looking for it maybe deleted
-              </p>
-              <a
-                href="/"
-                className="hover:text-primary inline-block rounded-lg border border-white px-8 py-3 text-center text-base font-semibold text-white transition hover:bg-white"
-              >
-                Go To Home
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="absolute top-0 left-0 -z-10 flex h-full w-full items-center justify-between space-x-5 md:space-x-8 lg:space-x-14">
-        <div className="h-full w-1/3 bg-gradient-to-t from-[#FFFFFF14] to-[#C4C4C400]"></div>
-        <div className="flex h-full w-1/3">
-          <div className="h-full w-1/2 bg-gradient-to-b from-[#FFFFFF14] to-[#C4C4C400]"></div>
-          <div className="h-full w-1/2 bg-gradient-to-t from-[#FFFFFF14] to-[#C4C4C400]"></div>
-        </div>
-        <div className="h-full w-1/3 bg-gradient-to-b from-[#FFFFFF14] to-[#C4C4C400]"></div>
-      </div>
-    </section>
+    <div class="h-screen w-screen bg-gray-100 flex items-center">
+	<div class="container flex flex-col md:flex-row items-center justify-center px-5 text-gray-700">
+   		<div class="max-w-md">
+      		<div class="text-5xl font-dark font-bold">404</div>
+            <p
+              class="text-2xl md:text-3xl font-light leading-normal"
+            >Sorry we couldn't find this page. </p>
+          <p class="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+          
+          <a href="/">
+          <button class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700">back to homepage</button>
+          </a>
+    </div>
+      <div class="max-w-lg">
+      <img
+            className="max-w-sm rounded-lg shadow-2xl"
+            src="https://seosherpa.com/wp-content/uploads/2020/12/404-error-page-header-transparent.png"
+            alt=""
+          />
+    </div>
+    
+  </div>
+</div>
   );
 };
 
--- src/Layout/pages/Gallery/Gallery.jsx
+++ src/Layout/pages/Gallery/Gallery.jsx
@@ -16,35 +16,35 @@ const Gallery = () => {
 
       <div className="container lg:px-32 px-4 py-8 mx-auto items-center ">
         <div className="grid grid-cols-4 grid-rows-4 grid-flow-col gap-2">
-          <div className="w-full row-span-2" data-aos = 'fade-right'>
+          <div className="w-full row-span-2" data-aos="fade-up">
             <img
               src="https://images.brickset.com/news/73742_Panther%202A.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
               className="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100"
             />
           </div>
-          <div className="w-full col-span-2 row-span-2">
+          <div className="w-full col-span-2 row-span-2" data-aos="fade-down">
             <img
               src="https://m.media-amazon.com/images/I/71TXFrNebXL.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
               className="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 "
             />
           </div>
-          <div className="w-full ">
+          <div className="w-full " data-aos="fade-right">
             <img
               src="https://cdn.dribbble.com/userupload/3569499/file/original-2b2f7c85628e027a073c58371a48c6c9.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
               className="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 "
             />
           </div>
-          <div className="w-full">
+          <div className="w-full" data-aos="fade-left">
             <img
               src="https://i.ebayimg.com/images/g/fpEAAOSwomdhhUiY/s-l1600.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
               className="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 "
             />
           </div>
-          <div className="w-full col-span-2 row-span-2">
+          <div className="w-full col-span-2 row-span-2" data-aos="fade-up-right">
             <img
               src="https://bleedingcool.com/wp-content/uploads/2018/11/LEGO-DC-Super-Villains-Aquaman-DLC-Portrait-1200x900.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
@@ -52,21 +52,21 @@ const Gallery = () => {
             />
           </div>
 
-          <div className="w-full col-span-2">
+          <div className="w-full col-span-2" data-aos="fade-up-left">
             <img
               src="https://img.artpal.com/423151/49-20-8-7-8-46-22m.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
               className="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 "
             />
           </div>
-          <div className="w-full">
+          <div className="w-full" data-aos="fade-down-right">
             <img
               src="https://cdnb.artstation.com/p/assets/images/images/046/080/449/large/fernando-chiriboga-r11bc-autofrotal.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
               className="inset-0 h-full w-full object-cover object-center rounded opacity-75 hover:opacity-100 "
             />
           </div>
-          <div className="w-full">
+          <div className="w-full" data-aos="fade-down-left">
             <img
               src="https://cdn.dribbble.com/users/2541618/screenshots/8139466/media/6ae2086a453e9aa666d398070ad18640.jpg"
               alt="Photo by Claudio Schwarz on Unsplash"
